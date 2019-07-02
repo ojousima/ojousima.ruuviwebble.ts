6037d31f5cb5be9cf9650348513beab4cda84538
Otso Jousimaa <otso@ojousima.net>
2019-07-02 17:38:18 +0300

    Add tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceComponent } from './device/device.component';
+import { RtcComponent } from './rtc/rtc.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceComponent,
+    RtcComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/rtc/rtc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rtc',
+  templateUrl: './rtc.component.html',
+  styleUrls: ['./rtc.component.css']
+})
+export class RtcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
